3d8c1ca0a66a739d6dc42e82d2708ba0933dfbdc
Liashevych Oleksandr <kleunodlav@gmail.com>
2023-10-11 18:39:36 +0300

    Add practical task #3 correct

---
+++ tests/api/test_fixtures.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.check
+def test_change_name(user):
+    assert user.name == 'Sergii'
+
+
+@pytest.mark.check
+def test_change_seconde_name(user):
+    assert user.second_name == 'Butenko'
