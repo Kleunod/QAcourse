1228e939a2bf2b1e0be53200015f58f55d20385a
Liashevych Oleksandr <kleunodlav@gmail.com>
2023-11-15 16:18:09 +0200

    Redo UI testing

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,38 +1,40 @@
 import pytest
 
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
 
 @pytest.mark.ui
 def test_check_incorrect_username():
     #Creating an object to control the browser
     driver = webdriver.Chrome(
-        service=Service(f'c:/Users/Alex/QAcourse/' + 'chromedriver.exe')
-        )
+        service=Service(ChromeDriverManager().install()))
 
     #open the page https://github.com/login
     driver.get ("https://github.com/login")
 
-    #We find the field in which we will enter the wrong username or postal address
-    login_element = driver.find_element(By.ID, "login_field")
+    #We find the field in which we will enter the wrong user name
+    login_elem = driver.find_element(By.ID, "login_field")
 
     #Enter an incorrect username or email address
-    login_element.send_keys("kleunodlav@mistakeinemail.com")
-    
+    login_elem.send_keys("kleunodlav@mistakeinemail.com")
+
     #We find the field in which we will enter the wrong password
     pass_elem = driver.find_element(By.ID, "password")
 
     #Enter the wrong password
     pass_elem.send_keys("wrong password")
 
-    #We find the button sign in
+    #We find the sign in button
     btn_elem = driver.find_element(By.NAME, "commit")
 
     #Emulate a click with the left mouse button
     btn_elem.click()
 
-    #We check that the name of the page is what we expect 
+    #We check that the name of the page is what we expect
     assert driver.title == "Sign in to GitHub · GitHub"
 
     #Close the browser
