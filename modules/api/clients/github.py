ef402bf59ea76ebfbb84618f5634c177f4640fe3
Liashevych Oleksandr <kleunodlav@gmail.com>
2023-11-15 09:54:08 +0200

    Remove cahe

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -11,8 +11,8 @@ class GitHub:
     
     def search_repo(self, name):
         r = requests.get(
-            "https://api.github.com/search/repositories",
-            params={"q": name}
+                        "https://api.github.com/search/repositories",
+                        params={"q": name}
         )
         body = r.json()
 
